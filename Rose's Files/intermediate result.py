1ac4a5cb45899f84689dae991ab971b1b14bc4e4
Anvar0526 <rustactic05@gmail.com>
2025-10-13 23:36:24 +0300

    Внесены изменения в файл Розы

--- Rose's Files/intermediate result.py
+++ Rose's Files/intermediate result.py
@@ -1,4 +1,5 @@
 # Simple Python code for test
-print("Hello, team! This is an intermediate result.")
-result = sum(range(1, 6))  # Сумма чисел от 1 до 5
+print("Hello, friends! This is an intermediate result.")
+result = sum(range(1, 7))  # Сумма чисел от 1 до 5
 print("Sum from 1 to 5:", result)
+#Внес изменения
