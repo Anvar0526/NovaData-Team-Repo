f261e6c63810a3eac9b69c326c9e1024f069ce71
AnatolyVdovikin <vdovikin@ro.ru>
2025-10-13 18:46:27 +0500

    Update Full editing.py
    Сделал "полное изменение" в файле Full editing.py

--- Anvar's Files/Full editing.py
+++ Anvar's Files/Full editing.py
@@ -6,15 +6,17 @@ from datetime import datetime, timedelta
 import random
 
 # Генерируем простые данные
-dates = [datetime.now() - timedelta(days=x) for x in range(10)]
-sales = [random.randint(100, 1000) for _ in range(10)]
+ids   = [i for _ in range(15)]
+dates = [datetime.now() - timedelta(days=x) for x in range(15)]
+sales = [random.randint(100, 1000) for _ in range(15)]
 
 data = {
+    'id': ids,
     'date': dates,
     'sales': sales,
-    'product': ['A', 'B'] * 5
+    'product': ['A', 'B'] * 255
 }
 
 df = pd.DataFrame(data)
 df.to_csv('data/sales_data.csv', index=False)
-print("Сгенерированы тестовые данные!")
+print("Сгенерированы новые тестовые данные!")
