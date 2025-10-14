4bc54f1cbc65e73c6c4cc7c708e41e824730b427
AnatolyVdovikin <vdovikin@ro.ru>
2025-10-14 13:31:23 +0500

    Update intermediate result.py
    Добавил комментарий

--- Rose's Files/intermediate result.py
+++ Rose's Files/intermediate result.py
@@ -6,3 +6,4 @@ print("Hello, friends! This is an intermediate result.")
 result = sum(range(1, 7))  # Сумма чисел от 1 до 5
 print("Sum from 1 to 5:", result)
 #Внес изменения
+#Я тоже внёс изменения
